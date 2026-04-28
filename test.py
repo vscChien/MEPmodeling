651da3d5c9709680b12000f041b194f7c688094e
EleBern <bernasconi@cs.cas.cz>
2026-04-28 15:14:08 +0200

    Check if output corresponds to matlab

--- test.py
+++ test.py
@@ -70,14 +70,53 @@ import matplotlib.pyplot as plt
 # from config_model_bio import config_model_bio
 # config_model_bio(1,1,0.5)
 #############################################################
-# from gen_kernels import gen_kernels
-# tlength = 50 # ms
-# dt = 0.1 # ms
-# gen_kernels(dt,tlength)
+from gen_kernels import gen_kernels
+from scipy.io import loadmat
+tlength = 50 # ms
+dt = 0.1 # ms
+AMPA, NMDA = gen_kernels(dt,tlength)
+
+m = loadmat("AMPA.mat")
+m = np.array(m["AMPA"])
+p = AMPA#np.loadtxt("AMPA.txt")
+
+print(np.nanmax(np.subtract(p,m)))
+print(np.nanargmax(np.subtract(p,m)))
+
+m = loadmat("NMDA.mat")
+m = np.array(m["NMDA"])
+p = NMDA#np.loadtxt("NMDA.txt")
+
+print(np.nanmax(np.subtract(p,m)))
+print(np.nanargmax(np.subtract(p,m)))
 #############################################################
-from generate_EP import generate_EP
-generate_EP(0.1, 1,2)
-generate_EP(0.1, 1)
+# from generate_EP import generate_EP
+# # generate_EP(0.1, 1,2)
+# generate_EP(0.1, 1)
+
+# import numpy as np
+# from scipy.io import loadmat
+# # m = loadmat("v.mat")
+# # m = np.array(m["v"])
+# # p = np.loadtxt("v.txt")
+# # print(np.allclose(m,np.round(p,4)))
+# # m = loadmat("dv.mat")
+# # m = np.array(m["dv"])
+# # p = np.loadtxt("dv.txt")
+# # print(np.allclose(m,p))
+# # m = loadmat("dvv.mat")
+# # m = np.array(m["ddv"])
+# # p = np.loadtxt("ddv.txt")
+# # print(np.allclose(m,np.round(p,4)))
+# m = loadmat("EP2.mat")
+# m = np.array(m["EP2"])
+# p = np.loadtxt("EP2.txt")
+# p = p.reshape(-1, 1)
+# print(np.shape(p))
+# print(np.shape(m))
+# print(np.max(np.subtract(p,m)))
+# print(np.argmax(np.subtract(p,m)))
+# print(np.allclose(m[:10],p[:10]))
 #############################################################
 # from load_muap import load_muap
 # load_muap(1)
@@ -88,24 +127,36 @@ generate_EP(0.1, 1)
 # load_MEP(subj, intensity_idx, [20, 50], 1)
 #############################################################
 # from gen_DIwave import gen_DIwave
-# from deconv_DIwave import deconv_DIwave
+# from deconv_DIwave2 import deconv_DIwave
 # import numpy as np
+# from scipy.io import loadmat
 
 # tlength = 50 # ms
 # dt = 0.1 # ms
 # t = np.arange(0, tlength, dt)
+# print(t[-1])
 # intensities=[29., 32., 35., 38., 41., 44., 47., 50., 53., 56.]
 # RMT=32
+# ref = {}
 # DIwave0=np.zeros((len(intensities), len(t)))
 # for i in range(len(intensities)):
 #     DIwave0[i,:] = gen_DIwave(t, intensities[i] / RMT)
-# DIwave = deconv_DIwave(t, DIwave0, ref={})
+# DIwave = deconv_DIwave(t, DIwave0, ref)
+# np.savetxt("DIwave.txt", DIwave)
 
-# plt.figure()
-# plt.plot(t, DIwave.T)
-# plt.grid(True)
-# plt.xlabel("Time (ms)")
-# plt.ylabel("Normalised mplitude")
-# plt.xlim([t[0], t[-1]])
+# # plt.figure()
+# # plt.plot(t, DIwave.T)
+# # plt.grid(True)
+# # plt.xlabel("Time (ms)")
+# # plt.ylabel("Normalised mplitude")
+# # plt.xlim([t[0], t[-1]])
 
-# plt.show()
+# # plt.show()
+
+
+# m = loadmat("DIwave.mat")
+# m = np.array(m["DIwave"])
+# p = np.loadtxt("DIwave.txt")
+
+# print(np.nanmax(np.subtract(p,m)))
+# print(np.nanargmax(np.subtract(p,m)))
